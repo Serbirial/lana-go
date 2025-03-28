77b7069fe842e044d841835a018af22df82b0209
Serbirial <39264658+Serbirial@users.noreply.github.com>
2025-03-27 22:02:45 -0400

    Update read.go
    See if this fixes a bug showing on raspbian

--- utils/fs/read.go
+++ utils/fs/read.go
@@ -3,10 +3,12 @@ package fs
 import (
 	"gobot/error"
 	"os"
+	"strings"
 )
 
 func ReadFileWhole(f string) string {
 	data, err := os.ReadFile(f)
 	error.ErrorCheckPanic(err)
-	return string(data)
+	dataString := string(data)
+	return strings.TrimSuffix(dataString, "\n")
 }
